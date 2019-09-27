9aab62d5420ab7115c3855dc38fe458ee8e15584
Danny Brito <britodanny1997@gmail.com>
2019-09-27 14:53:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,9 +40,9 @@ end
 
 def reduce_to_any_true(source_array)
   source_array.length.times{|index|
-  if(source_array[index])
+    if(source_array[index])
     return true
-  end }
+    end }
   return false
 end 
 
